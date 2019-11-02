509c9c0074250413489e6fad9551cfef8f4a2293
Pawel Adamczuk <p.m.adamczuk@gmail.com>
2019-11-02 16:16:29 -0700

    Initial commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace InfiniNotes
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Scintilla = new ScintillaNET.Scintilla();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.tsslSaving = new System.Windows.Forms.ToolStripStatusLabel();
+            this.SaveTimer = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Scintilla
+            // 
+            this.Scintilla.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Scintilla.Location = new System.Drawing.Point(0, 0);
+            this.Scintilla.Name = "Scintilla";
+            this.Scintilla.Size = new System.Drawing.Size(800, 450);
+            this.Scintilla.TabIndex = 0;
+            this.Scintilla.Text = "scintilla1";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsslSaving});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 408);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 42);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // tsslSaving
+            // 
+            this.tsslSaving.Name = "tsslSaving";
+            this.tsslSaving.Size = new System.Drawing.Size(79, 32);
+            this.tsslSaving.Text = "Saved";
+            // 
+            // SaveTimer
+            // 
+            this.SaveTimer.Enabled = true;
+            this.SaveTimer.Interval = 5000;
+            this.SaveTimer.Tick += new System.EventHandler(this.SaveTimer_Tick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.Scintilla);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ScintillaNET.Scintilla Scintilla;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel tsslSaving;
+        private System.Windows.Forms.Timer SaveTimer;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace InfiniNotes
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Scintilla.SavePointReached += Scintilla_SavePointReached;
+            Scintilla.SavePointLeft += Scintilla_SavePointLeft;
+            Scintilla.TextChanged += Scintilla_TextChanged;
+
+            if(!File.Exists(@"c:\tmp\tmp.txt"))
+#pragma warning disable CS0642 // Possible mistaken empty statement
+                using (File.Create(@"c:\tmp\tmp.txt")) ;
+#pragma warning restore CS0642 // Possible mistaken empty statement
+
+            using (StreamReader sr = new StreamReader(@"c:\tmp\tmp.txt", Encoding.UTF8))
+            {
+                var fileContent = sr.ReadToEnd();
+                Scintilla.Text = fileContent;
+                Scintilla.SetSavePoint();
+            }
+
+            SaveTimer.Start();
+        }
+
+        private void Scintilla_SavePointLeft(object sender, EventArgs e)
+        {
+            tsslSaving.Text = "Dirty";
+        }
+
+        private void Scintilla_SavePointReached(object sender, EventArgs e)
+        {
+            tsslSaving.Text = "Saved";
+        }
+
+        private void Scintilla_TextChanged(object sender, EventArgs e)
+        {
+            SaveTimer.Stop();
+            SaveTimer.Start();
+        }
+
+        private async void SaveTimer_Tick(object sender, EventArgs e)
+        {
+            if (!Scintilla.Modified)
+                return;
+
+            SaveTimer.Enabled = false;
+
+            var filePath = @"c:\tmp\tmp.txt";
+            byte[] encodedText = Encoding.UTF8.GetBytes(Scintilla.Text);
+
+            using (FileStream sourceStream = new FileStream(filePath,
+            FileMode.Create, FileAccess.Write, FileShare.None,
+            bufferSize: 4096, useAsync: true))
+            {
+                await sourceStream.WriteAsync(encodedText, 0, encodedText.Length);
+            };
+
+            Scintilla.SetSavePoint();
+            SaveTimer.Enabled = true;
+        }
+    }
+}
